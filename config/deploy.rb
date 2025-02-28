d09689e590ddcd8da9fdc18f353ba90ccb6c2608
Chris Blackburn <8dd969a2@opayq.com>
2025-02-28 14:38:02 -0700

    Fix asset compiles on prod

--- config/deploy.rb
+++ config/deploy.rb
@@ -63,7 +63,7 @@ set :keep_releases, 5
 # Uncomment the following to require manually verifying the host key before first deploy.
 # set :ssh_options, verify_host_key: :secure
 
-# after 'deploy:updated', 'assets:compile'
+after 'deploy:updated', 'assets:compile'
 namespace :assets do
   task :compile do
     on roles(:app) do
