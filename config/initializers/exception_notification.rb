ff18ddc1707b0b0a35f632063e6e7faf79b638f5
Chris Blackburn <8dd969a2@opayq.com>
2025-03-14 14:21:56 -0600

    Fix sidekiq issues

--- config/initializers/exception_notification.rb
+++ config/initializers/exception_notification.rb
@@ -2,9 +2,9 @@
 
 # Move this require to your `config/application.rb` if you want to be notified from runner commands too.
 require 'exception_notification/rails'
-require 'exception_notification/rake'
 
 # NOTE: This doesn't work in production
+# require 'exception_notification/rake'
 # require 'exception_notification/sidekiq'
 
 ExceptionNotification.configure do |config|
