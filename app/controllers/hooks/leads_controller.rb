9952a242210cd5fa1e7cf7eabd1fed6565e78084
Chris Blackburn <8dd969a2@opayq.com>
2025-03-25 11:50:10 -0600

    Update form-config

--- app/controllers/hooks/leads_controller.rb
+++ app/controllers/hooks/leads_controller.rb
@@ -3,26 +3,58 @@
 # This webhook is for incoming leads of all types
 class Hooks::LeadsController < WebhookController
   ALLOWED_PARAMS = {
-    'step_2_-_military_status' => 'Military Status string',
-    'step_3_-_marital_status' => 'Marital Status string',
-    'step_4_-_how_much_coverage' => 'Needed Coverage string',
-    'step_6_-_best_time_to_contact' => 'Contact Time string',
+    address: 'Address string',
+    adset_id: 'Adset ID - string',
+    amt_requested: 'Amount requested string',
+    beneficiary: 'Beneficiary type - example: Spouse - string',
+    beneficiary_name: 'Name of beneficiaries string',
+    branch_of_service: 'Branch of military service - string',
+    campaign_id: 'Campaign ID - string',
+    city: 'City - string',
+    contact_time_of_day: 'Best time to contact you string',
+    current_retirement_plan: 'Current retirement plan - string',
+    desired_monthly_contrib: 'Monthly contribution - string',
+    desired_retirement_age: 'Desired retirement age - string',
     dob: 'Date of Birth string - example: "12-01-1943" - REQUIRED',
     email: 'Email string - REQUIRED',
+    employment_status: 'Employment status - string',
+    external_lead_id: 'ELID - string',
+    favorite_hobby: 'Favorite hobby - string',
     fbc_id: 'Facebook ID string',
     fbclid: 'Facebook Client ID string',
     first_name: 'First Name string - REQUIRED',
     full_name: 'Full Name string',
+    gender: 'Gender string',
+    google_click_id: 'Google click ID - string',
     h_ad_id: 'Ad ID string',
+    has_life_insurance: 'Has life insurance - string',
+    health_history: 'Any history of cancer, heart attack, etc - string',
     ip_address: 'IP Address string',
     is_dropoff: 'Dropoff string',
+    iul_goal: 'IUL goal - string',
     last_name: 'Last Name string - REQUIRED',
     lead_class: 'Lead Class Name string - "VeteranLeadPremium", etc. - REQUIRED',
+    lead_form_name: 'Lead form name - string',
+    lead_order_id: 'Lead order ID - string',
+    lead_program: 'Lead program - string',
+    location: 'Location - string',
+    marital_status: 'Marital Status string',
+    military_status: 'Military Status string',
+    monthly_contribution: 'Monthly contribution - string',
+    mortgage_balance: 'Mortgage balance - string',
+    mortgage_payment: 'Mortgage payment - string',
+    needed_coverage: 'How much coverage do you need string',
+    otp_code: 'OTP code - string',
+    outside_company: 'Outside company - string',
+    page_url: 'Page URL - string',
     parent_url: 'Parent URL string',
     phone: 'Phone string - at least 10 digits - REQUIRED',
+    platform: 'Platform - string',
     ref_id: 'Referal ID',
+    retirement_age: 'Retirement age - string',
     start: 'Unknown string',
     state: 'Lead State string - REQUIRED',
+    trusted_form_url: 'TFU - string',
     user_agent: 'User Agent string',
     utm_adset: 'UTM Adset string',
     utm_campaign: 'UTM Campaign string',
@@ -33,7 +65,8 @@ class Hooks::LeadsController < WebhookController
     utm_site_source: 'UTM Site Source string',
     utm_source: 'UTM Source string',
     utm_term: 'UTM Term string',
-    video_type: 'Video Type string - *Other or Dom'
+    video_type: 'Video Type string - *Other or Dom',
+    zip: 'Zipcode - string'
   }.freeze
 
   # POST /hooks/leads
@@ -53,3 +86,8 @@ class Hooks::LeadsController < WebhookController
     params.require(:form).permit(ALLOWED_PARAMS.keys)
   end
 end
+
+{
+  'form' => { 'ref_id' => 2371544,
+'parent_url' => 'https://quote.unitedseniorsavingsorg.com/?utm_source=fb&utm_medium=Facebook_Mobile_Feed&utm_campaign=FFL+Jobs+|+FEX+Premium+Campaign+(Video+and+Image)+|+State+Specific&utm_content=Nevada+-+Dynamic+Images+%26+Video&utm_adset=Nevada&utm_site_source=fb&utm_owner=RoundRobin&fbc_id=120220207455950655&h_ad_id=120220207455940655&fbclid=IwZXh0bgNhZW0BMABhZGlkAascPbOV7y8BHRz6PL7Hene9gvZFUQwh_LUyakRn4D95XpoamjuaGlHb8iYgcQ4FQI_tKQ_aem_GaD99molZri9TRGdp_ICug&utm_id=120219512347280655&utm_term=120220207455950655', 'ip_address' => '70.173.92.210', 'user_agent' => 'Mozilla/5.0 (iPhone; CPU iPhone OS 18_1_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/22B91 [FBAN/FBIOS;FBAV/499.0.0.53.105;FBBV/693867054;FBDV/iPhone15,3;FBMD/iPhone;FBSN/iOS;FBSV/18.1.1;FBSS/3;FBID/phone;FBLC/en_US;FBOP/5;FBRV/697301994;IABMV/1]', 'is_dropoff' => false, 'utm_source' => 'fb', 'utm_medium' => 'Facebook_Mobile_Feed', 'utm_campaign' => 'FFL Jobs | FEX Premium Campaign (Video and Image) | State Specific', 'utm_content' => 'Nevada - Dynamic Images & Video', 'utm_adset' => 'Nevada', 'utm_site_source' => 'fb', 'utm_owner' => 'RoundRobin', 'fbc_id' => '120220207455950655', 'h_ad_id' => '120220207455940655', 'fbclid' => 'IwZXh0bgNhZW0BMABhZGlkAascPbOV7y8BHRz6PL7Hene9gvZFUQwh_LUyakRn4D95XpoamjuaGlHb8iYgcQ4FQI_tKQ_aem_GaD99molZri9TRGdp_ICug', 'utm_id' => '120219512347280655', 'utm_term' => '120220207455950655', 'state' => 'Nevada', 'step_2_-_gender' => 'Female', 'date_of_birth' => '05-20-1964', 'step_4_-_have_life_insurance' => 'No', 'step_5_-_how_much_coverage' => '$15,000', 'step_6_-_who_will_be_the_beneficiary' => 'My Children', 'beneficiary_name' => 'Andrea Duran', 'step_8_-_medical_question' => 'Yes', 'favorite_hobby' => 'Crafting', 'full_name' => 'Barbara Duran', 'email' => 'barbaraduran.1964@gmail.com', 'phone' => '+17023387532', 'xxtrustedformcerturl' => 'https://cert.trustedform.com/34d6f7ca4069099b022da889babc04689a47b6cb' }, 'lead' => {}
+}
