7c883d1a91ea175d9c06cd20290a7c22d87f1957
Chris Blackburn <8dd969a2@opayq.com>
2025-04-02 11:59:40 -0600

    Allow ad-spend summaries to update

--- app/controllers/hooks/ad_daily_spend_summaries_controller.rb
+++ app/controllers/hooks/ad_daily_spend_summaries_controller.rb
@@ -2,8 +2,20 @@
 
 # This webhook is for incoming daily ad spend
 class Hooks::AdDailySpendSummariesController < WebhookController
+  before_action :find_existing_summary
+
   # POST /hooks/ad_daily_spend_summaries
   def create
+    if @existing_summary.present?
+      update_existing_summary
+    else
+      create_new_summary
+    end
+  end
+
+  private
+
+  def create_new_summary
     summary = AdDailySpendSummary.new(ad_spend_params)
     if summary.save
       head :created
@@ -15,9 +27,27 @@ class Hooks::AdDailySpendSummariesController < WebhookController
     end
   end
 
-  private
+  def update_existing_summary
+    if @existing_summary.update(ad_spend_params)
+      head :ok
+    else
+      Rails.logger.error(
+        ">>> Failed to update AdSpendDailySummary: - #{@existing_summary&.errors&.full_messages}"
+      )
+      head :unprocessable_content
+    end
+  end
 
   def ad_spend_params
     params.expect(ad_daily_spend_summary: %i[date lead_type platform campaign ad_spend])
+  end
+
+  def find_existing_summary
+    @existing_summary = AdDailySpendSummary.find_by(
+      date: ad_spend_params[:date],
+      lead_type: ad_spend_params[:lead_type],
+      platform: ad_spend_params[:platform],
+      campaign: ad_spend_params[:campaign]
+    )
   end
 end
--- spec/requests/hooks/ad_spend_summaries_spec.rb
+++ spec/requests/hooks/ad_spend_summaries_spec.rb
@@ -13,24 +13,56 @@ module Hooks
     end
 
     describe 'POST /hooks/ad_daily_spend_summaries' do
-      context 'with valid params' do
-        it 'returns a created response' do
-          post url, params: valid_params
-          expect(response).to have_http_status(:created)
+      context 'when creating a new summary' do
+        context 'with valid params' do
+          it 'returns a created response' do
+            post url, params: valid_params
+            expect(response).to have_http_status(:created)
+          end
+
+          it 'creates a AdDailySpendSummary' do
+            expect do
+              post url, params: valid_params
+            end.to change(AdDailySpendSummary, :count).by(1)
+          end
         end
 
-        it 'creates a AdDailySpendSummary' do
-          expect do
+        context 'with invalid params' do
+          it 'returns a unprocessable response' do
+            valid_params[:ad_daily_spend_summary][:lead_type] = nil
             post url, params: valid_params
-          end.to change(AdDailySpendSummary, :count).by(1)
+            expect(response).to have_http_status(:unprocessable_content)
+          end
         end
       end
 
-      context 'with invalid params' do
-        it 'returns a unprocessable response' do
-          valid_params[:ad_daily_spend_summary][:lead_type] = nil
-          post url, params: valid_params
-          expect(response).to have_http_status(:unprocessable_content)
+      context 'when updating an existing summary' do
+        let(:valid_params) do
+          {
+            ad_daily_spend_summary: summary.attributes
+          }
+        end
+
+        context 'with valid params' do
+          it 'returns a success response' do
+            post url, params: valid_params
+            expect(response).to have_http_status(:ok)
+          end
+
+          it 'does not create a new AdDailySpendSummary' do
+            expect(summary).to be_present
+            expect do
+              post url, params: valid_params
+            end.not_to change(AdDailySpendSummary, :count)
+          end
+        end
+
+        context 'with invalid params' do
+          it 'returns a unprocessable response' do
+            valid_params[:ad_daily_spend_summary][:ad_spend] = nil
+            post url, params: valid_params
+            expect(response).to have_http_status(:unprocessable_content)
+          end
         end
       end
     end
